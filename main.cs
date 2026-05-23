2b23c72382d302e6594cd11b8515b3d12d1be3c1
KimTaeYeon <109526205+kty2006@users.noreply.github.com>
2026-05-23 14:14:56 +0000

    use

--- main.cs
+++ main.cs
@@ -1,13 +1,26 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {   
+        int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        int n = input[0];
+        int a = input[1];
+        int b = input[2];
+        int count = b - 2;
+        
+        if(count >= n)  
+        {
+          int loofCount  =  (count % n) == 0 ? 1 : count / n;
 
-        
-        
-       
+          Console.Write(loofCount);
+        }
+        else
+        {
+            Console.Write(count);
+        }
 
     }
 }
